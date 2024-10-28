62d9dca1e2a840c08965a94b9167ba3452098019
ADE39 <947200536@qq.com>
2024-10-28 13:15:51 +0800

    提交文件

--- vue-element/src/main.js
+++ vue-element/src/main.js
@@ -4,8 +4,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import * as ElIcons from '@element-plus/icons-vue';
 const app = createApp(App)
 
-app.use(router)
+// 注册Element Plus图标
+for (const [key, component] of Object.entries(ElIcons)) {
+    app.component(key, component);
+  }
 
+app.use(router)
+app.use(ElementPlus);
 app.mount('#app')
